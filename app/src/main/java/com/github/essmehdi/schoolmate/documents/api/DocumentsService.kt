18d83c8f64c6b3be8b94c4eba6e35c29d5b35e9a
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-04-18 02:37:28 +0000

    refactor: fixed some warning & class rename

--- app/src/main/java/com/github/essmehdi/schoolmate/documents/api/DocumentsService.kt
+++ app/src/main/java/com/github/essmehdi/schoolmate/documents/api/DocumentsService.kt
@@ -1,12 +1,11 @@
 package com.github.essmehdi.schoolmate.documents.api
 
-import com.github.essmehdi.schoolmate.documents.api.dto.UploadDocumentDto
+import com.github.essmehdi.schoolmate.documents.api.dto.DocumentDetailsDto
 import com.github.essmehdi.schoolmate.documents.models.Document
 import com.github.essmehdi.schoolmate.documents.models.DocumentTag
 import com.github.essmehdi.schoolmate.shared.api.dto.MessageResponse
 import com.github.essmehdi.schoolmate.shared.api.dto.PaginatedResponse
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -20,10 +19,10 @@ interface DocumentsService {
 
   @POST("documents")
   @Multipart
-  fun uploadDocument(@Part("data") uploadDocumentDto: UploadDocumentDto, @Part file: MultipartBody.Part): Call<Document>
+  fun uploadDocument(@Part("data") documentDetailsDto: DocumentDetailsDto, @Part file: MultipartBody.Part): Call<Document>
 
   @PATCH("documents/{id}")
-  fun editDocument(@Path("id") id: Long, @Body uploadDocumentDto: UploadDocumentDto): Call<Document>
+  fun editDocument(@Path("id") id: Long, @Body documentDetailsDto: DocumentDetailsDto): Call<Document>
 
   @HEAD("documents/{id}/file")
   fun downloadDocumentHeaders(@Path("id") id: Long): Call<Void>
