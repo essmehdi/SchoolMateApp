683fbcdc0e807fab203d5ea586667c11bb1cbfdf
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-04-27 02:52:20 +0100

    feat: implemented user registration

--- app/src/main/java/com/github/essmehdi/schoolmate/auth/api/AuthService.kt
+++ app/src/main/java/com/github/essmehdi/schoolmate/auth/api/AuthService.kt
@@ -2,6 +2,7 @@ package com.github.essmehdi.schoolmate.auth.api
 
 import com.github.essmehdi.schoolmate.auth.api.dto.LoginDto
 import com.github.essmehdi.schoolmate.auth.api.dto.LoginResponse
+import com.github.essmehdi.schoolmate.auth.api.dto.RegisterDto
 import com.github.essmehdi.schoolmate.auth.models.User
 import retrofit2.Call
 import retrofit2.http.Body
@@ -13,6 +14,10 @@ interface AuthService {
   @POST("login")
   @Headers("Content-Type: application/json")
   fun login(@Body loginDto: LoginDto): Call<LoginResponse>
+
+  @POST("users")
+  @Headers("Content-Type: application/json")
+  fun register(@Body registerDto: RegisterDto): Call<User>
 
   @GET("me")
   fun me(): Call<User>
