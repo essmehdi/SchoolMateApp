f49ee13d48ec6093e0360e32104d6c5c8be744b6
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-04-30 16:03:23 +0100

    feat: enhanced school navigation ui

--- app/src/main/java/com/github/essmehdi/schoolmate/schoolnavigation/ui/SchoolNavigationActivity.kt
+++ app/src/main/java/com/github/essmehdi/schoolmate/schoolnavigation/ui/SchoolNavigationActivity.kt
@@ -1,46 +1,73 @@
 package com.github.essmehdi.schoolmate.schoolnavigation.ui
 
 import android.Manifest
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.StringRes
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.preference.PreferenceManager
 import com.github.essmehdi.schoolmate.R
-import com.github.essmehdi.schoolmate.schoolnavigation.models.SchoolZone
-import com.github.essmehdi.schoolmate.shared.api.Api
+import com.github.essmehdi.schoolmate.databinding.ActivitySchoolNavigationBinding
+import com.github.essmehdi.schoolmate.schoolnavigation.viewmodels.SchoolNavigationViewModel
+import com.github.essmehdi.schoolmate.shared.api.BaseResponse
 import com.github.essmehdi.schoolmate.shared.utils.GeoUtils
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.mapsforge.core.graphics.Color
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Polygon
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class SchoolNavigationActivity : AppCompatActivity() {
 
   private lateinit var map: MapView
-  private var schoolZones = emptyList<SchoolZone>()
+  private val viewModel: SchoolNavigationViewModel by viewModels()
+  private lateinit var binding: ActivitySchoolNavigationBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_school_navigation)
+    binding = ActivitySchoolNavigationBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+      isAppearanceLightNavigationBars = true
+      isAppearanceLightStatusBars = true
+    }
+
+    listOf(binding.schoolNavigationBackButton, binding.schoolNavigationRefreshCard).forEach {
+      ViewCompat.setOnApplyWindowInsetsListener(it) { view, windowInsets ->
+        val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        view.updateLayoutParams<MarginLayoutParams> {
+          topMargin = insets.top + 5
+        }
+
+        WindowInsetsCompat.CONSUMED
+      }
+    }
 
     // Force light mode temporarily
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
     // Initialize map
     Configuration.getInstance().load(applicationContext, PreferenceManager.getDefaultSharedPreferences(applicationContext))
@@ -93,88 +120,93 @@ class SchoolNavigationActivity : AppCompatActivity() {
       }
     }
 
-    // Fetch school zones
-    GlobalScope.launch {
-      try {
-        val response = Api.schoolZonesService.getAllSchoolZones()
-        if (response.isSuccessful) {
-          schoolZones = response.body() ?: emptyList()
-          populateMap()
-        } else {
-          Toast.makeText(this@SchoolNavigationActivity, R.string.school_zones_loading_failed, Toast.LENGTH_LONG).show()
-          Log.e("API Status Code", response.code().toString())
-        }
-      } catch (e: Exception) {
-        e.printStackTrace()
+    viewModel.fetchStatus.observe(this) {
+      showLoadingIndicator(it is BaseResponse.Loading)
+      if (it is BaseResponse.Error) {
+        handleError(it.code!!)
       }
+    }
+
+    viewModel.schoolZones.observe(this) { populateMap() }
+
+    binding.schoolNavigationBackButton.setOnClickListener {
+      onBackPressedDispatcher.onBackPressed()
     }
   }
 
-  fun initMap() {
-    map = findViewById(R.id.map)
-    map.setTileSource(TileSourceFactory.MAPNIK)
-    map.setMultiTouchControls(true) // Enables pinch to zoom & other gestures
-    map.minZoomLevel = 19.2 // Zoom on ENSIAS
-    map.mapOrientation = -37.5f
-
-    val controller = map.controller
-    controller.setZoom(19.2)
-    // Set center to ENSIAS
-    val startPoint = GeoPoint(33.98429267486034, -6.8675806261125905)
-    controller.setCenter(startPoint)
-  }
-
-  fun populateMap() {
-    // Create for each zone a polygon in the map
-    val polygons: List<Polygon> = schoolZones.map { schoolZone ->
-      // Map coordinates to GeoPoints
-      val geopoints = schoolZone.geometry.points.map { GeoPoint(it.x, it.y) }
-
-      val polygon = Polygon()
-      polygon.apply {
-        setOnClickListener(Polygon.OnClickListener { _, mapView, eventPos ->
-          val fragment = SchoolZoneDetailsFragment.newInstance(schoolZone.name, schoolZone.description)
-          fragment.show(supportFragmentManager, fragment.tag)
-
-          return@OnClickListener true
+  private fun showLoadingIndicator(show: Boolean = true) {
+    Log.d("SchoolNavActivity", "Show loading indicator: $show")
+    binding.schoolNavigationRefreshCard.apply {
+      animate()
+        .setInterpolator(AccelerateDecelerateInterpolator())
+        .alpha(if (show) 1f else 0f)
+        .setListener(object : AnimatorListenerAdapter() {
+          override fun onAnimationEnd(animation: Animator) {
+            super.onAnimationEnd(animation)
+            isVisible = show
+          }
         })
-        fillColor = ResourcesCompat.getColor(resources, R.color.zone, theme)
-        strokeWidth = 1f
-        points = geopoints
-        title = schoolZone.name
+    }
+  }
+
+  private fun initMap() {
+    map = binding.map
+    map.apply {
+      setTileSource(TileSourceFactory.MAPNIK)
+      setMultiTouchControls(true) // Enables pinch to zoom & other gestures
+      minZoomLevel = 19.2 // Zoom on ENSIAS
+      mapOrientation = -37.5f
+    }
+
+    map.controller.apply {
+      setZoom(19.2)
+      // Set center to ENSIAS
+      setCenter(GeoPoint(33.98429267486034, -6.8675806261125905))
+    }
+  }
+
+  /**
+   * Populates map with fetched zones polygons
+   */
+  private fun populateMap() {
+    viewModel.schoolZones.value?.forEach {
+      it.apply {
+        setOnClickListener { _, _, _ ->
+          val fragment = SchoolZoneDetailsFragment.newInstance(it.title, it.subDescription)
+          fragment.show(supportFragmentManager, fragment.tag)
+          true
+        }
+        fillPaint.apply {
+          color = ResourcesCompat.getColor(resources, R.color.primary, theme)
+        }
+        outlinePaint.apply {
+          color = ResourcesCompat.getColor(resources, R.color.primary_variant, theme)
+          strokeWidth = 1f
+        }
       }
 
-      return@map polygon
-    }
-    // Draw all polygons in the map overlay
-    polygons.forEach {
-      // Text marker for the polygon
-      val marker = Marker(map)
-      marker.position = GeoUtils.centerOfPolygon(it.actualPoints)
-      marker.title = it.title
-      marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
-      marker.textLabelBackgroundColor = Color.TRANSPARENT.ordinal
-      marker.setTextIcon(it.title)
-
+      val marker = Marker(map).apply {
+        position = GeoUtils.centerOfPolygon(it.actualPoints)
+        setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+        textLabelBackgroundColor = Color.TRANSPARENT.ordinal
+        setTextIcon(it.title)
+      }
       map.overlayManager.addAll(listOf(it, marker))
     }
   }
 
-  fun enableLocation() {
+  /**
+   * Enables location in the map
+   */
+  private fun enableLocation() {
     Toast.makeText(this, "Enabling location...", Toast.LENGTH_SHORT).show()
     val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), map)
     locationOverlay.enableMyLocation()
     map.overlayManager.add(locationOverlay)
   }
 
-  fun showMessageDialog(@StringRes title: Int, @StringRes message: Int, @StringRes neutralButtonText: Int) {
-    AlertDialog.Builder(this@SchoolNavigationActivity).apply {
-      setTitle(title)
-      setMessage(message)
-      setNeutralButton(neutralButtonText) { dialogInterface, _ ->
-        dialogInterface.dismiss()
-      }
-    }
+  private fun handleError(code: Int) {
+    Toast.makeText(this, R.string.unknown_error_occurred, Toast.LENGTH_LONG).show()
   }
 
   override fun onResume() {
---
+++ app/src/main/java/com/github/essmehdi/schoolmate/schoolnavigation/viewmodels/SchoolNavigationViewModel.kt
@@ -0,0 +1,54 @@
+package com.github.essmehdi.schoolmate.schoolnavigation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import com.github.essmehdi.schoolmate.schoolnavigation.models.SchoolZone
+import com.github.essmehdi.schoolmate.shared.api.Api
+import com.github.essmehdi.schoolmate.shared.api.BaseResponse
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Polygon
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SchoolNavigationViewModel: ViewModel() {
+  val fetchStatus: MutableLiveData<BaseResponse<List<SchoolZone>>> = MutableLiveData()
+  val schoolZones: LiveData<List<Polygon>> = fetchStatus.map { fetchedZones ->
+    if (fetchedZones is BaseResponse.Success) {
+      fetchedZones.data!!.map { fetchedZone ->
+        val geoPoints = fetchedZone.geometry.points.map { GeoPoint(it.x, it.y) }
+        val polygon = Polygon().apply {
+          points = geoPoints
+          title = fetchedZone.name
+          subDescription = fetchedZone.description
+        }
+        polygon
+      }
+    } else {
+      listOf()
+    }
+  }
+
+  init {
+    fetchZones()
+  }
+
+  private fun fetchZones() {
+    fetchStatus.value = BaseResponse.Loading()
+    Api.schoolZonesService.getAllSchoolZones().enqueue(object: Callback<List<SchoolZone>> {
+      override fun onResponse(call: Call<List<SchoolZone>>, response: Response<List<SchoolZone>>) {
+        if (response.isSuccessful) {
+          fetchStatus.value = BaseResponse.Success(response.body()!!)
+        } else {
+          fetchStatus.value = BaseResponse.Error(response.code())
+        }
+      }
+
+      override fun onFailure(call: Call<List<SchoolZone>>, t: Throwable) {
+        fetchStatus.value = BaseResponse.Error(0)
+      }
+    })
+  }
+}
