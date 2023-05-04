3c861a9f355a448832b37f6eefbe051c28ea457b
MouadFiali <fialimouad@gmail.com>
2023-05-04 01:57:28 +0100

    feat: created and initiated new activities for complaints details and editing

---
+++ app/src/main/java/com/github/essmehdi/schoolmate/complaints/ui/ComplaintDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.essmehdi.schoolmate.complaints.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.essmehdi.schoolmate.R
+
+class ComplaintDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complaint_details)
+    }
+}
---
+++ app/src/main/java/com/github/essmehdi/schoolmate/complaints/ui/ComplaintEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.github.essmehdi.schoolmate.complaints.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.essmehdi.schoolmate.R
+
+class ComplaintEditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complaint_editor)
+    }
+}
