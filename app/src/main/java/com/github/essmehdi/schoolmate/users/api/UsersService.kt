eec4d8dc006a747c9d13c6d540b6a9222a1b169e
Mehdi Essalehi <mehdi.essalehi@gmail.com>
2023-05-09 18:28:51 +0100

    feat: added pagination to users list

--- app/src/main/java/com/github/essmehdi/schoolmate/users/api/UsersService.kt
+++ app/src/main/java/com/github/essmehdi/schoolmate/users/api/UsersService.kt
@@ -1,13 +1,23 @@
 package com.github.essmehdi.schoolmate.users.api
 
+import com.github.essmehdi.schoolmate.shared.api.dto.PaginatedResponse
 import com.github.essmehdi.schoolmate.users.models.User
+import com.github.essmehdi.schoolmate.users.models.UserRole
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface UsersService {
   @GET("users")
-  fun getAllUsers(): Call<List<User>>
+  fun getAllUsers(
+    @Query("page") page: Long? = 0,
+    @Query("size") size: Int? = 30,
+    @Query("sort") sort: String? = "lastName,desc",
+    @Query("email") email: String? = null,
+    @Query("search") search: String? = null,
+    @Query("role") role: UserRole? = null
+  ): Call<PaginatedResponse<User>>
 
   @GET("users/{id}")
   fun getUserById(@Path("id") id: Long): Call<User>
